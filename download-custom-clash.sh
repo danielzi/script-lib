2c58d9e0e4a0f58a8b687ecdb2216b296693389f
daniel <danielzi@qq.com>
2024-05-30 13:58:11 +0800

    Delete download-custom-clash.sh

--- download-custom-clash.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd Xboard/
-docker-compose exec -it xboard sh << EOF
-cd resources/rules/
-wget https://gist.githubusercontent.com/danielzi/5dc31457f5f0b1b64814b14028b1c0cb/raw/custom.clash.yaml
-exit
-EOF
